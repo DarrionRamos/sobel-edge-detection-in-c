4edfe67710d34ffec237d9d2662bf72c9a747832
ethanm-debug <ethan.mendoza@ufl.edu>
2025-03-23 14:15:44 -0400

    Update modified_sobel.c

--- modified_sobel.c
+++ modified_sobel.c
@@ -105,21 +105,6 @@ int convolution(pgm* image, int kernel[3][3], int row, int col) {
 
 void sobel_edge_detector(pgm* image, pgm* out_image) {
 	int i, j, gx, gy;
-	int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-	int8_t *counts = (int8_t*)calloc(size, sizeof(int8_t));
-	int8_t *displ = (int8_t*)calloc(size, sizeof(int8_t));
-	int sum = 0;
-	int8_t rows = image->height/size;
-	int8_t rem = image->height%size;
-
-	for(int i = 0; i < size; i++) {
-		counts[i] = (rows + (i < rem? 1 : 0));
-		displ[i] = sum;
-		sum += counts[i];
-	}
-
 	int mx[3][3] = {
 		{-1, 0, 1},
 		{-2, 0, 2},
@@ -130,11 +115,9 @@ void sobel_edge_detector(pgm* image, pgm* out_image) {
 		{0, 0, 0},
 		{1, 2, 1}
 	};
-	//#pragma omp parallel for private(i,j)
-	//each rank gets their own for loop iteration 
-	//this approach is a lot cleaner because we dont have to change much
-	for (int i = displ[rank]; i < displ[rank]+counts[rank]; i++) {
-		for (int j = 1; j < image->width - 2; j++) {
+	
+	for (i = 1; i < image->height - 2; i++) {
+		for (j = 1; j < image->width - 2; j++) {
 			gx = convolution(image, mx, i, j);
 			gy = convolution(image, my, i, j);
 			out_image->imageData[i][j] = sqrt(gx*gx + gy*gy);
@@ -144,40 +127,47 @@ void sobel_edge_detector(pgm* image, pgm* out_image) {
 	}
 	
 }
-//the nasty sobel filter that doesnt work that well
-/*
-void sobel_edge_detector(pgm* image, pgm* out_image) {
+
+/*void sobel_edge_detector(pgm* image, pgm* out_image) {
+	int i, j, gx, gy;
 	int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-	int *counts = (int*)malloc(sizeof(int)*size);
-	int *displ = (int*)malloc(sizeof(int)*size);
-
-	int *count_conv = (int*)malloc(sizeof(int)*size);
-	int *displ_conv = (int*)malloc(sizeof(int)*size);
-
-	int *convo_x = (int*)malloc(sizeof(int)*4997*4997);
-	int *convo_y = (int*)malloc(sizeof(int)*4997*4997);
-	int *final =  (int*)malloc(sizeof(int)*image->height*image->width);
-	
-	memset(final, 0, sizeof(int)*image->height*image->width);
-	memset(convo_x, 0, sizeof(int)*4997*4997);
-	memset(convo_y, 0, sizeof(int)*4997*4997);
-	int i, j, gx, gy;
-	int rows = (image->height*image->width)/size;
-	int rem = (image->height*image->width)%size;
-	int my_row = rows + (rank < rem ? 1 : 0);
+	int8_t *counts = (int8_t*)calloc(size, sizeof(int8_t));
+	int8_t *displ = (int8_t*)calloc(size, sizeof(int8_t));
 	int sum = 0;
-	for(int i = 0; i < size; i++) 
+	int8_t rows = (image->height-2)/size;
+	int8_t rem = (image->height-2)%size;
+	int height = image->height, width = image->width;
+	int rows_proc = height/size;
+	int cols = width;
+	int comp_arr[height][width];
+	int main[height][width];
+
+
+	MPI_Scatter(main, (rows_proc*height), MPI_INT, comp_arr, rows_proc*height, MPI_INT, 0, MPI_COMM_WORLD);
+	for(int i = 1; i < height+1; i++)
 	{
-		counts[i] = my_row;
-		displ[i] = sum;
-		sum += counts[i];
+		for (int j = 0; j < rows_proc + 1; j++) 
+		{
+			int gx = (comp_arr[i-1][j-1]*-1 + comp_arr[i+1][j-1]*1 + comp_arr[i-1][j]*-2 + comp_arr[i+1][j]*2 + comp_arr[i-1][j+1]*-1 + comp_arr[i+1][j+1]*1);
+			int gy = (comp_arr[i-1][j-1]*1 + comp_arr[i][j-1]*2 + comp_arr[i+1][j-1]*1 
+					+ comp_arr[i-1][j+1]*-1 + comp_arr[i][j+1]*-2 + comp_arr[i+1][j+1]*-1 + comp_arr[i+1][j+1]*1);
+			//printf("%d", main[i][j]);
+			int grad = gx*gx +
+			image->imageData[i][j] = 
+		}
 	}
 
-	int *local_buf = (int*)malloc(sizeof(int)*counts[rank]);
-	memset(local_buf, 0, sizeof(int)*counts[rank]);
-	int mx[3][3] = {
+	//MPI_Scatter();
+*/
+	/*for(int i = 0; i < size; i++) {
+		counts[i] = (rows + (i < rem? 1 : 0));
+		displ[i] = sum;
+		sum += counts[i];
+	}*/
+
+	/*int mx[3][3] = {
 		{-1, 0, 1},
 		{-2, 0, 2},
 		{-1, 0, 1}
@@ -187,42 +177,19 @@ void sobel_edge_detector(pgm* image, pgm* out_image) {
 		{0, 0, 0},
 		{1, 2, 1}
 	};
-	MPI_Scatterv(final, counts, displ, MPI_INT, 
-		local_buf, counts[rank], MPI_INT, 
-		0, MPI_COMM_WORLD);
-	for (i = 1; i < image->height - 2; i++) {
-	//storing the values of convolution into a 1d array
-	//didn't really like this approach
-		for (j = 1; j < image->width - 2; j++) {
-			gx = convolution(image, mx, i, j);
-			convo_x[i * (image->width) + j] = gx;
-			gy = convolution(image, my, i, j);
-			convo_y[i * (image->width) + j] = gy;
+	*/
+	//#pragma omp parallel for private(i,j)
+	
 
-		}
-	}
-	for(int i = displ[rank]; i < displ[rank] + counts[rank]; i++)
-	{	
-		local_buf[i] = sqrt(convo_x[i]*convo_x[i] + convo_y[i]*convo_y[i]);
-		
-	}
-
-	MPI_Gatherv(local_buf, counts[rank], MPI_INT,
-	final, counts, displ, MPI_INT, 0, MPI_COMM_WORLD);
-
-	if (rank==0)
-	{
-		for (i = 1; i < image->height - 2; i++) {
-			for (j = 1; j < image->width - 2; j++) {
-				out_image->imageData[i][j] = final[(i*image->width)+j];
-				
-			}
-		}
-	}
-
-}//end sobel
-*/
-
+	//for (i = 1; i < image->height - 2; i++) {
+	//	for (j = 1; j < image->width - 2; j++) {
+			//gx = convolution(image, mx, i, j);
+			//gy = convolution(image, my, i, j);
+			//a[i*image->width + j] = sqrt(gx*gx + gy*gy);
+	//	}
+	//}
+	
+//}
 
 void min_max_normalization(pgm* image, int8_t** matrix) {
 	int min = 1000000, max = 0, i, j;
@@ -288,6 +255,9 @@ void write_pgm_file(pgm* image, char dir[], int8_t** matrix, char name[]) {
 int main(int argc, char **argv)
 {
 	MPI_Init(&argc,&argv);
+	int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 	pgm image, out_image;
 	char dir[200];
 	printf("Enter the file name: ");
@@ -296,13 +266,26 @@ int main(int argc, char **argv)
 	read_pgm_file(dir, &image);
 	padding(&image);
 	init_out_image(&out_image, image);
-	sobel_edge_detector(&image, &out_image);
+	int proc_rows = image.height/size;
+
+	pgm local;
+	//num rows to send
+	local.height = proc_rows;
+
+	MPI_Scatter(image.imageData, proc_rows, MPI_INT,
+				local.imageData, proc_rows, MPI_INT, 0, MPI_COMM_WORLD);
+	sobel_edge_detector(&local, &out_image);
+
+	MPI_Gather(local.imageData, proc_rows, MPI_INT,
+			image.imageData, proc_rows, MPI_INT, 0 , MPI_COMM_WORLD);
 
 	min_max_normalization(&out_image, out_image.imageData);
 	min_max_normalization(&out_image, out_image.gx);
 	min_max_normalization(&out_image, out_image.gy);
 
+
 	write_pgm_file(&out_image, dir, out_image.imageData, "_filtered.txt");
+
 	printf("\nGradient saved: %s \n", dir);
 //	write_pgm_file(&out_image, dir, out_image.gx, ".GX.pgm");
 //	printf("Gradient X saved: %s \n", dir);
