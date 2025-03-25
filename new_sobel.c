b3459f7515cd855c7a240ca386f659a1b712d944
ethanm-debug <ethan.mendoza@ufl.edu>
2025-03-24 21:28:37 -0400

    Update new_sobel.c

--- new_sobel.c
+++ new_sobel.c
@@ -6,11 +6,12 @@
 #include <time.h>
 #include <omp.h>
 
+int isspace(int argument);
+
 int rank, size;
-int i, j;
 int col = 5000, row = 5000;
 
-void read_file(char* dir, int8_t* data )
+void read_file(char* dir, int* data)
 {
     FILE* file;
     file = fopen(dir, "rb");
@@ -19,44 +20,30 @@ void read_file(char* dir, int8_t* data )
         printf("File could not open\n");
         return;
     }
+    printf("opened file: %s\n", dir);
     int num;
-    for(int i = 0; i < col; i++)
-    {
-        for(int j = 0; j < row; j++)
-        {
-            fscanf(file, "%d", &num);
-            data[i*row+j] = num;
-            printf("%d ", num);
-        }
+    for (int i = 0; i < row*col; i++) {
+	fscanf(file, "%d", &num);
+	data[i] = num;
+	printf("%d,%d     ", data[i], num);
+	if (i % row == 0)
+		printf("\n");
     }
     fclose(file);
 }
 
-void padding(int8_t* image_data) {
-	int i;
-	for (i = 0; i < row; i++) {
-		image_data[0*row+i] = 0;
-		image_data[(col - 1)*row+i] = 0;
-	}
-
-	for (i = 0; i < col; i++) {
-		image_data[0*col+i] = 0;
-		image_data[(row - 1)*col+i] = 0;
-	}
-}
-
-int convolution(int8_t* image_data, int kernel[3][3], int row_l, int col_l) {
+int convolution(int* image_data, int kernel[3][3], int row_l, int col_l) {
 	int i, j, sum = 0;
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 3; j++) {
-			sum += image_data[(i+row_l)*3+(j+col_l)] * kernel[i][j];
-			//printf("Image data at: i: %d, j: %d, is: %d\n", i+row_l, j+col_l, image_data[(i+row_l)*3+(j*col_l)]);
+			int index = i*3 + row_l + j + col_l;
+			sum += image_data[index] * kernel[i][j];
 		}
 	}
 	return sum;
 }
 
-void sobel_edge_detector(int8_t* in_image, int8_t* out_image) {
+void sobel_edge_detector(int* in_image, int* out_image) {
 	int i, j, gx, gy;
 	int mx[3][3] = {
 		{-1, 0, 1},
@@ -68,57 +55,31 @@ void sobel_edge_detector(int8_t* in_image, int8_t* out_image) {
 		{0, 0, 0},
 		{1, 2, 1}
 	};
-
-	for (i = 1; i < col/size - 2; i++) {
-		for (j = 1; j < row - 2; j++) {
-			gx = convolution(in_image, mx, i, j);
-			gy = convolution(in_image, my, i, j);
-			out_image[i*(row-2)+j] = sqrt(gx*gx + gy*gy);
-			//if (i == 4)
-				//printf("i: %d, j: %d, gx: %d, gy: %d\n", i, j, gx, gy);
+	#pragma omp parallel for private(i,j)
+	for (i = 1; i < row/size - 2; i++) {
+		for (j = 1; j < col - 2; j++) {
+			gx = convolution(in_image, mx, i*5000, j);
+			gy = convolution(in_image, my, i*5000, j);
+			out_image[i*(col)+j] = sqrt((gx*gx + gy*gy));
 		}
 	}
-}
+}	
 
-void min_max_normalization(int8_t* image_data) {
-	int min = 1000000, max = 0;
-
-    for(i = 0; i < col/size; i++) {
-		for(j = 0; j < row; j++) {
-			if (image_data[i*row+j] < min) {
-				min = image_data[i*row+j] ;
-			}
-			else if (image_data[i*row+j] > max) {
-				max = image_data[i*row+j];
-			}
-		}
-	}
-
-    for(i = 0; i < col/size; i++) {
-		for(j = 0; j < row; j++) {
-			if (image_data[i*row+j]  > min + 70) {
-				if (image_data[i*row+j] + 30 < 255)
-                image_data[i*row+j] = image_data[i*row+j] + 30;
-				else
-                image_data[i*row+j] = 255;
-			}
-		}
-	}
-}
-
-void write_file(int8_t* out_data, char dir[]) {
+void write_file(int* out_data, char dir[]) {
     FILE* out_file;
+	int i, j;
     char* token = strtok(dir, ".");
-	if (token != NULL) {
-		strcat(token, "_filtered.txt");
-		out_file = fopen(token, "wb");
-	}
+    if (token != NULL) {
+	strcat(token, "_filtered.txt");
+	out_file = fopen(token, "wb");
+    }
     out_file = fopen(dir, "wb");
-    for(int i = 0; i < col; i++)
+	//#pragma omp parallel for private(i,j)
+    for(i = 0; i < row; i++)
     {
-        for(int j = 0; j < row; j++) 
+        for(j = 0; j < col; j++)
         {
-            fprintf(out_file, "%d     ", out_data[i*row+j]);
+            fprintf(out_file, "%d     ", (u_int8_t)out_data[i*col+j]);
         }
         fprintf(out_file, "\n");
     }
@@ -127,26 +88,58 @@ void write_file(int8_t* out_data, char dir[]) {
 
 int main(int argc, char** argv)
 {
+    int i, j;
     char dir[200];
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int8_t* image_data = (int8_t*)calloc(col*row, sizeof(int8_t));
-    int8_t* out_data = (int8_t*)calloc(col*row, sizeof(int8_t));
+    int* image_data = (int*)calloc(col*row, sizeof(int));
+    int* out_data = (int*)calloc(col*row, sizeof(int));
 
-    //if(rank==0)
-    //{
-    //strcpy(dir, argv[1]);
-    read_file("input.txt", image_data);
-	//}
+	int *count = (int*)calloc(row*col, sizeof(int));
+	int *displ = (int*)calloc(row*col, sizeof(int));
+	int num_per_proc = row/size;
+	int remainder = row%size;
+	int rows_per_proc = num_per_proc + (rank < remainder?1:0);
+	//printf("%d %d %d", num_per_proc, remainder, rows_per_proc);
 
-    sobel_edge_detector(image_data, out_data);
+	int *local_buf = (int*)calloc(rows_per_proc*col, sizeof(int));
+	int *out_buf = (int*)calloc(rows_per_proc*col, sizeof(int));
 
-    min_max_normalization(image_data);
+	clock_t start = clock();
 
-    //if (rank == 0)
-    write_file(out_data, dir);
+    if(rank==0)
+    {
+		strcpy(dir, argv[1]);
+		FILE* file;
+		file = fopen("input.txt", "rb");
+		if(file == NULL)
+		{
+			printf("File could not open\n");
+			return -1;
+		}
+		printf("opened file: %s\n", dir);
+		int num;
+		//#pragma omp parallel for private(i)
+		for (int i = 0; i < row*col; i++) {
+			fscanf(file, "%d", &num);
+			image_data[i] = num;
+			//printf("%d ", image_data[i]);
+		}
+		fclose(file);
+	}
+	int temp = 0;
 
+	MPI_Scatter(image_data, num_per_proc*col, MPI_INT, local_buf, num_per_proc*col, MPI_INT, 0, MPI_COMM_WORLD);
+    sobel_edge_detector(local_buf, out_buf);
+	MPI_Gather(out_buf, num_per_proc*col, MPI_INT, out_data, num_per_proc*col, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    	write_file(out_data, dir);
+
+	clock_t end = clock();
+	if(rank==0)
+	printf("Program time: %.5f sec\n", ((double)(end-start))/CLOCKS_PER_SEC);
     free(image_data);
     free(out_data);
 
